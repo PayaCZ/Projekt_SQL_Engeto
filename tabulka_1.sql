64c7c84b919ac2967c91e451d1ca05a309c0478b
PayaCZ <78.78@seznam.cz>
2023-11-29 06:00:38 +0100

    edit table_1

--- tabulka_1.sql
+++ tabulka_1.sql
@@ -7,17 +7,20 @@
 CREATE TABLE t_pavel_smid_projekt_SQL_primary_final AS
 SELECT 
 	cpay.payroll_year, 
-	cpay.value AS value_payroll, 
+	round(avg(cpay.value), 2) AS value_payroll, 
 	cpay.industry_branch_code, 
 	cpib.name AS branch_name, 
-	cp.value, 
+	round(avg(cp.value), 2) AS value, 
 	cp.category_code, 
 	cpc.name AS food_name
 FROM czechia_payroll cpay
 LEFT JOIN czechia_price cp 
 	ON cpay.payroll_year = YEAR(cp.date_from)
 	AND cpay.value_type_code = 5958
+	AND cpay.unit_code = 200
+	AND cpay.industry_branch_code IS NOT NULL	 
 LEFT JOIN czechia_payroll_industry_branch cpib 
 	ON cpay.industry_branch_code = cpib.code
 LEFT JOIN czechia_price_category cpc 
-	ON cp.category_code = cpc.code;
+	ON cp.category_code = cpc.code
+GROUP BY cpay.payroll_year, cpay.industry_branch_code, cpib.name, cp.category_code, cpc.name;
